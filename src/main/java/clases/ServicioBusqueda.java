78c474ecfb2dde53d8f8d0607ce4e5c28230f0cb
Sebastian Puerta <sepuertabe@gmail.com>
2024-12-27 18:00:33 -0500

    El método calcularDescuento de ServicioBusqueda fue renombrado a obtenerPrecioPorFecha y fue refactorizado

--- src/main/java/clases/ServicioBusqueda.java
+++ src/main/java/clases/ServicioBusqueda.java
@@ -39,7 +39,7 @@ public class ServicioBusqueda {
 
             if (habitacionMasBarata != null) {
                 double precioHabitacion = habitacionMasBarata.getPrecio();
-                double precioFinalConDescuento = calcularDescuento(precioHabitacion, fechaInicioAlojamiento, fechaFinalAlojamiento, numeroDeHabitacionesAReservar);
+                double precioFinalConDescuento = obtenerPrecioPorFecha(precioHabitacion, fechaInicioAlojamiento, fechaFinalAlojamiento, numeroDeHabitacionesAReservar);
 
                 System.out.println("Nombre del establecimiento: " + hotel.getNombre());
                 System.out.println("Tipo de alojamiento: " + hotel.getTipoAlojamiento());
@@ -60,30 +60,64 @@ public class ServicioBusqueda {
     }
 
     // Este es un método auxiliar para calcular el descuento/aumento según las fechas. Es privado para que no sea usado desde otra clase.
-    private double calcularDescuento(double precioHabitacion, String fechaInicioAlojamiento, String fechaFinalAlojamiento, int numeroDeHabitacionesAReservar) {
-        // Convertir las fechas a LocalDate
-        LocalDate inicio = LocalDate.parse(fechaInicioAlojamiento);
-        LocalDate fin = LocalDate.parse(fechaFinalAlojamiento);
+    private double obtenerPrecioPorFecha(double precioHabitacion, String fechaInicioAlojamiento, String fechaFinalAlojamiento, int numeroDeHabitacionesAReservar) {
+        LocalDate inicio = transformarAFecha(fechaInicioAlojamiento);
+        LocalDate fin = transformarAFecha(fechaFinalAlojamiento);
 
-        // Calcular el número total de días de estadía
         long diasEstadia = ChronoUnit.DAYS.between(inicio, fin);
 
-        // Calcular el precio base total
         double precioTotal = precioHabitacion * diasEstadia * numeroDeHabitacionesAReservar;
 
-        // Validar los días y aplicar ajustes
         int diaInicio = inicio.getDayOfMonth();
         int diaFin = fin.getDayOfMonth();
 
-        if (diaFin > 25) {
-            precioTotal *= 1.15;
-        } else if (diaInicio >= 10 && diaFin <= 15) {
-            precioTotal *= 1.10;
-        } else if (diaInicio >= 5 && diaFin <= 10) {
-            precioTotal *= 0.92;
-        }
+        return calcularPrecioFinal(precioTotal, diaInicio, diaFin);
+    }
 
+    private LocalDate transformarAFecha(String fecha) {
+        return LocalDate.parse(fecha);
+    }
+
+    private double calcularPrecioFinal(double precioTotal, int diaInicio, int diaFin) {
+        final double RECARGO_LATE = 1.15;
+        final double DESCUENTO_MEDIO_MES = 1.10;
+        final double DESCUENTO_INICIO_MES = 0.92;
+
+        if (isEndOfMonth(diaFin)) {
+            return precioFinalModificado(precioTotal, RECARGO_LATE);
+        }
+        if (isHalfMonth(diaInicio, diaFin)) {
+            return precioFinalModificado(precioTotal, DESCUENTO_MEDIO_MES);
+        }
+        if (isStartOfMonth(diaInicio, diaFin)) {
+            return precioFinalModificado(precioTotal, DESCUENTO_INICIO_MES);
+        }
         return precioTotal;
+    }
+
+    private boolean isEndOfMonth (int diaFin){
+        if (diaFin > 25) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isHalfMonth (int diaInicio, int diaFin) {
+        if (diaInicio >= 10 && diaFin <= 15) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isStartOfMonth (int diaInicio, int diaFin) {
+        if (diaInicio >= 5 && diaFin <= 10) {
+            return true;
+        }
+        return false;
+    }
+
+    private double precioFinalModificado (double precioFinal, double recargo){
+        return precioFinal*recargo;
     }
 
     public List<Habitacion> confirmarHabitaciones(Hotel hotel, String fechaInicioAlojamiento,String fechaFinalAlojamiento, int numeroDeAdultos, int numeroDeNiños, int numeroDeHabitacionesAReservar) {
@@ -96,7 +130,7 @@ public class ServicioBusqueda {
         for (Habitacion habitacion : hotel.getHabitaciones()) {
             resultadosHabitaciones.add(habitacion);
 
-            precioFinalConDescuento = calcularDescuento(habitacion.getPrecio(), fechaInicioAlojamiento, fechaFinalAlojamiento, numeroDeHabitacionesAReservar);
+            precioFinalConDescuento = obtenerPrecioPorFecha(habitacion.getPrecio(), fechaInicioAlojamiento, fechaFinalAlojamiento, numeroDeHabitacionesAReservar);
             System.out.println("Tipo: " + habitacion.getTipo()
                     + " | Características: " + habitacion.getCaracteristicas()
                     + " | Precio por habitación: $" + String.format("%.2f", habitacion.getPrecio())
