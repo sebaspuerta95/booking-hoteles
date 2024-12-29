2aa8cf33588116a848989ebdfef939fb12c70b13
Sebastian Puerta <sepuertabe@gmail.com>
2024-12-29 18:22:27 -0500

    UserInteractionService was renamed and translated to English, it was also moved to utils package

--- src/main/java/inicioApp/Main.java
+++ src/main/java/inicioApp/Main.java
@@ -2,6 +2,7 @@ package src.main.java.inicioApp;
 
 import src.main.java.clases.*;
 import src.main.java.utils.Database;
+import src.main.java.utils.UserInteractionService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +19,7 @@ public class Main {
 
         System.out.println("Bienvenido a Booking Hoteles!");
         while (!salir) {
-            int opcion = InteraccionUsuario.solicitarOpcion("""
+            int opcion = UserInteractionService.askMenuOption("""
                     \nSeleccione una opción:
                     1. Buscar hoteles y reservar
                     2. Actualizar reservation
@@ -37,33 +38,33 @@ public class Main {
     }
 
     private static void buscarYReservarHotel() {
-        String ciudad = InteraccionUsuario.solicitarCadena("Ingrese la ciudad donde desea alojarse: ");
-        String tipoAlojamiento = InteraccionUsuario.solicitarCadena("Ingrese el tipo de alojamiento (Hotel, Apartamento, Finca o Día de Sol): ");
-        String fechaInicio = InteraccionUsuario.solicitarCadena("Ingrese la fecha de inicio de su estadía (yyyy-MM-dd): ");
-        String fechaFinal = InteraccionUsuario.solicitarCadena("Ingrese la fecha final de su estadía (yyyy-MM-dd): ");
-        int adultos = InteraccionUsuario.solicitarEntero("Ingrese la cantidad de adultos: ");
-        int ninos = InteraccionUsuario.solicitarEntero("Ingrese la cantidad de niños: ");
-        int habitaciones = InteraccionUsuario.solicitarEntero("Ingrese la cantidad de habitaciones: ");
+        String ciudad = UserInteractionService.requestStringToUser("Ingrese la ciudad donde desea alojarse: ");
+        String tipoAlojamiento = UserInteractionService.requestStringToUser("Ingrese el tipo de alojamiento (Hotel, Apartamento, Finca o Día de Sol): ");
+        String fechaInicio = UserInteractionService.requestStringToUser("Ingrese la fecha de inicio de su estadía (yyyy-MM-dd): ");
+        String fechaFinal = UserInteractionService.requestStringToUser("Ingrese la fecha final de su estadía (yyyy-MM-dd): ");
+        int adultos = UserInteractionService.requestIntegerToUser("Ingrese la cantidad de adultos: ");
+        int ninos = UserInteractionService.requestIntegerToUser("Ingrese la cantidad de niños: ");
+        int habitaciones = UserInteractionService.requestIntegerToUser("Ingrese la cantidad de habitaciones: ");
 
-        List<Hotel> opciones = servicioBusqueda.buscarHoteles(hoteles, ciudad, tipoAlojamiento, fechaInicio, fechaFinal, adultos, ninos, habitaciones);
+        List<Hotel> availableHotels = servicioBusqueda.buscarHoteles(hoteles, ciudad, tipoAlojamiento, fechaInicio, fechaFinal, adultos, ninos, habitaciones);
 
-        if (opciones.isEmpty()) {
+        if (availableHotels.isEmpty()) {
             System.out.println("No se encontraron hoteles que cumplan con los criterios.");
             return;
         }
 
-        hotelSeleccionado = InteraccionUsuario.seleccionarHotel("Seleccione el número del hotel que desea reservar:", opciones);
-        List<Room> habitacionesDisponibles = servicioBusqueda.confirmarHabitaciones(hotelSeleccionado, fechaInicio, fechaFinal, adultos, ninos, habitaciones);
+        hotelSeleccionado = UserInteractionService.selectHotelToBook("Seleccione el número del hotel que desea reservar:", availableHotels);
+        List<Room> availableRooms = servicioBusqueda.confirmarHabitaciones(hotelSeleccionado, fechaInicio, fechaFinal, adultos, ninos, habitaciones);
 
-        if (habitacionesDisponibles.isEmpty()) {
+        if (availableRooms.isEmpty()) {
             System.out.println("No se encontraron habitaciones disponibles para las fechas seleccionadas.");
             return;
         }
 
-        Room roomSeleccionada = InteraccionUsuario.seleccionarHabitacion("Seleccione el número de la habitación que desea reservar:", habitacionesDisponibles);
+        Room roomSeleccionada = UserInteractionService.selectRoomToBook("Seleccione el número de la habitación que desea reservar:", availableRooms);
 
-        Client client = InteraccionUsuario.solicitarCliente();
-        String horaAproxLlegada  = InteraccionUsuario.solicitarCadena("Hora aproximada de llegada: ");
+        Client client = UserInteractionService.requestClientsInformation();
+        String horaAproxLlegada  = UserInteractionService.requestStringToUser("Hora aproximada de llegada: ");
 
         List<Room> habitacionesSeleccionadas = new ArrayList<>();
         habitacionesSeleccionadas.add(roomSeleccionada);
@@ -73,8 +74,8 @@ public class Main {
     }
 
     private static void actualizarReserva() {
-        String email = InteraccionUsuario.solicitarCadena("Para actualizar tu reservation actual, primero debemos validar tu identidad. \nIngresa tu email: ");
-        String fechaNacimiento = InteraccionUsuario.solicitarCadena("Ingrese su fecha de nacimiento (yyyy-MM-dd): ");
+        String email = UserInteractionService.requestStringToUser("Para actualizar tu reservation actual, primero debemos validar tu identidad. \nIngresa tu email: ");
+        String fechaNacimiento = UserInteractionService.requestStringToUser("Ingrese su fecha de nacimiento (yyyy-MM-dd): ");
 
         if (hotelSeleccionado == null) {
             System.out.println("No hay reservas realizadas aún.");
---
+++ src/main/java/utils/UserInteractionService.java
@@ -0,0 +1,111 @@
+package src.main.java.utils;
+
+import src.main.java.clases.Client;
+import src.main.java.clases.Hotel;
+import src.main.java.clases.Room;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class UserInteractionService {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String requestStringToUser(String message) {
+        System.out.print(message);
+        return scanner.nextLine();
+    }
+
+    public static int requestIntegerToUser(String message) {
+        System.out.print(message);
+        return scanner.nextInt();
+    }
+
+    public static Hotel selectHotelToBook(String message, List<Hotel> availableHotels) {
+        System.out.println(message);
+        for (int i = 0; hotelsListOptionLimit(i, availableHotels); i++) {
+            printHotelsOptionsList(i, availableHotels);
+        }
+
+        int selection = requestIntegerToUser("Ingrese el número de su elección: ") - 1;
+        int hotelsOptionsSize = availableHotels.size();
+        if (isSelectionValid(selection, hotelsOptionsSize)) {
+            System.out.println("Selección inválida. Inténtelo de nuevo.");
+            return selectHotelToBook(message, availableHotels);
+        }
+
+        return availableHotels.get(selection);
+    }
+
+    private static boolean hotelsListOptionLimit(int i, List<Hotel> hotelsOptions){
+        return i < hotelsOptions.size();
+    }
+
+    private static void printHotelsOptionsList(int i, List<Hotel> hotelsOptions){
+        Hotel hotel = hotelsOptions.get(i);
+        System.out.println((i + 1) + ". " + hotel.getHotelName());
+    }
+
+    private static boolean isSelectionValid(int selection, int hotelsOptionsSize){
+        return selection < 0 || selection >= hotelsOptionsSize;
+    }
+
+    public static Room selectRoomToBook(String message, List<Room> availableRooms) {
+        System.out.println(message);
+        for (int i = 0; roomsListOptionLimit(i, availableRooms); i++) {
+            printRoomsOptionList(i, availableRooms);
+        }
+
+        int selectin = requestIntegerToUser("Ingrese el número de su elección: ") - 1;
+        int roomsOptionsSize = availableRooms.size();
+        if (isSelectionValid(selectin, roomsOptionsSize)) {
+            System.out.println("Selección inválida. Inténtelo de nuevo.");
+            return selectRoomToBook(message, availableRooms);
+        }
+
+        return availableRooms.get(selectin);
+    }
+
+    private static boolean roomsListOptionLimit(int i, List<Room> roomsOptions){
+        return i < roomsOptions.size();
+    }
+
+    private static void printRoomsOptionList(int i, List<Room> roomsOptions) {
+        Room room = roomsOptions.get(i);
+        System.out.println((i + 1) + ". " + room.getRoomType());
+    }
+
+    public static Client requestClientsInformation() {
+        scanner.nextLine();
+        String clientFirstname = requestStringToUser("Ingrese su nombre: ");
+        String clientLastname = requestStringToUser("Ingrese su apellido: ");
+        String clientDateOfBirth = requestStringToUser("Ingrese su fecha de nacimiento (yyyy-MM-dd): ");
+        String clientEmail = requestStringToUser("Ingrese su email: ");
+        String clientNationality = requestStringToUser("Ingrese su nacionalidad: ");
+        String clientPhoneNumber = requestStringToUser("Ingrese su número de teléfono: ");
+
+        return new Client(
+                clientFirstname,
+                clientLastname,
+                clientDateOfBirth,
+                clientEmail,
+                clientNationality,
+                clientPhoneNumber);
+    }
+
+    public static int askMenuOption(String message, int minimumOption, int maximumOption) {
+        System.out.print(message);
+        int menuOption = scanner.nextInt();
+        scanner.nextLine();
+        if (isValidMenuOption(menuOption, minimumOption, maximumOption)) {
+            System.out.println("Opción inválida. Inténtelo de nuevo.");
+            return askMenuOption(message, minimumOption, maximumOption);
+        }
+        return menuOption;
+    }
+
+    private static boolean isValidMenuOption(int menuOption, int minimumOption, int maximumOption) {
+        return menuOption < minimumOption || menuOption > maximumOption;
+    }
+
+}
+
